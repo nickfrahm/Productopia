2bca3593bcf4926ac48165a00a407b44aca26ea9
Nick Frahm <nfrahm24@gmail.com>
2021-02-14 19:24:31 -0600

    need to figure out how to make the tabs look nice

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -4,7 +4,20 @@ import React from 'react'
 function Pomodoro() {
   return (
     <div className="pomodoro">
-      
+      <div className="row break-tab-row">
+        <p>Work</p>
+        <p>Short Break</p>
+        <p>Long Break</p>
+      </div>
+      <div className="row timer-row">
+        <p>time</p>
+      </div>
+      <div className="row start-stop-row">
+        <p>Start</p>
+        <p>Stop</p>
+        <p>Reset</p>
+        <p>Settings</p>
+      </div>
     </div>
   );
 }
