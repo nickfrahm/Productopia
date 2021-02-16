e0ff2845d7d0212d2cd5ab1a6a39b3019a943b16
Nick Frahm <nfrahm24@gmail.com>
2021-02-16 17:56:57 -0600

    Working on Menu

---
+++ src/components/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Menu() {
+    return (
+      <div className="menu">
+        <ul>
+            <li>
+                Pomodoro
+            </li>
+            <li>
+                Note Taker
+            </li>
+            <li>
+                Goal Tracker
+            </li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Menu;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Menu from './Menu'
 
 function Nav() {
+
+  const [showMenu, setShowMenu] = useState('false');
+
+  function handleNavClick(e) {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <div className="nav">
       <h1 className="nav-item">Productopia</h1>
-      <FontAwesomeIcon icon={faBars} className="nav-item" id="menu"/>
+      <FontAwesomeIcon 
+        icon={faBars} 
+        className="nav-item" id="menu-icon"
+        onClick={handleNavClick}  
+      />
+      <div className={showMenu ? 'hideMenu' : 'showMenu'}>
+        <Menu />
+      </div>
     </div>
   );
 }
