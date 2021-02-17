d413d7805386f289dda94c66d7076bd402469fa9
Nick Frahm <nfrahm24@gmail.com>
2021-02-17 07:58:59 -0600

    working on pomodoro styling an added more state

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 function Pomodoro() {
+  const [workTime, setWorkTime] = useState('25:00');
+  const [activeTab, setActiveTab] = useState("work");
+  
+  function handleWorkBreakClick(e) {
+    setActiveTab(e.target.id);
+  }
+
   return (
     <div className="pomodoro">
       <div className="row break-tab-row">
-        <p>Work</p>
-        <p>Short Break</p>
-        <p>Long Break</p>
+        <p id="work" className={activeTab == "work" ? 'active' : ''} onClick={handleWorkBreakClick}>Work</p>
+        <p id="short" className={activeTab == "short" ? 'active' : ''} onClick={handleWorkBreakClick}>Short Break</p>
+        <p id="long" className={activeTab == "long" ? 'active' : ''} onClick={handleWorkBreakClick}>Long Break</p>
       </div>
       <div className="row timer-row">
         <p>time</p>
