8f46746d42d25bcd3a6023c1a645c5322f1aa749
Nick Frahm <nfrahm24@gmail.com>
2021-02-17 19:02:41 -0600

    Trying to get parent state to update

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,15 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Timer from './Timer'
-
 
 function Pomodoro() {
   const [activeTab, setActiveTab] = useState("work");
-  const [timerOnOff, setTimerOnOff] = useState(false);
-  
+  const [timerOn, setTimerOn] = useState(false);
+  const [timerLength, setTimerLength] = useState(2000);
+
   function handleWorkBreakClick(e) {
     setActiveTab(e.target.id);
   }
 
+  function handleTimerStart() {
+    setTimerOn(true);
+  }
+
+  function handleTimerStop() {
+    setTimerOn(false);
+  }
+
+  function handleReset() {
+    setTimerLength(60000);
+  }
+
+  function handleTimerChange(newLength) {
+    setTimerLength(newLength);
+  }
   return (
     <div className="pomodoro">
       <div className="row break-tab-row">
@@ -18,12 +33,12 @@ function Pomodoro() {
         <p id="long" className={activeTab == "long" ? 'active' : 'inactive'} onClick={handleWorkBreakClick}>Long Break</p>
       </div>
       <div className="row timer-row">
-        <Timer activeTab={activeTab} isOn={timerOnOff} />
+        <Timer timerOn={timerOn} timerLength={timerLength} handleChange={handleTimerChange} />
       </div>
       <div className="row start-stop-row">
-        <p style={{backgroundColor: '#81c784'}}>Start</p>
-        <p style={{backgroundColor: '#e57373'}}>Stop</p>
-        <p style={{backgroundColor: '#fff176'}}>Reset</p>
+        <p style={{backgroundColor: '#81c784'}} onClick={handleTimerStart}>Start</p>
+        <p style={{backgroundColor: '#e57373'}} onClick={handleTimerStop}>Stop</p>
+        <p style={{backgroundColor: '#fff176'}} onClick={handleReset}>Reset</p>
         <p style={{backgroundColor: '#e0e0e0'}}>Settings</p> 
       </div>
     </div>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,11 +1,39 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
-function Timer(activeTab, isOn) {
-    const [time, setTime] = useState()
+function Timer(props) {
+    const [seconds, setSeconds] = useState(props.timerLength);
+
+    useEffect(() => {
+        setSeconds(props.timerLength);
+    },[props.timerLength]);
+
+    useEffect(() => {
+        let interval = null;
+        if (props.timerOn && seconds > 0) {
+            interval = setInterval(() => {
+                setSeconds(seconds => seconds - 1000);
+            }, 1000);
+        } else if (seconds < 0) {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval);
+    }, [props.timerOn, seconds]);
+
+    function convertToReadableTime(time) {
+        let minutes = 0;
+        let secs = time/1000;
+
+        while (secs - 59 > 0) {
+            secs -= 60;
+            minutes++;
+        }
+
+        return (minutes < 10 ? '0' + minutes : minutes) + ":" + (secs < 10 ? '0' + secs : secs);
+    }
 
     return (
       <div className="timer">
-        25:00
+        {convertToReadableTime(seconds)}
       </div>
     );
   }
